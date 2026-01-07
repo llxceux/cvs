260b5988f28393b5ded19e85eb17163f6894ed9e
llxceux <llxceux@gmail.com>
2026-01-08 06:01:48 +0700

    Update adminroot.php

--- shell/adminroot.php
+++ shell/adminroot.php
@@ -59,7 +59,7 @@ $default_timezone = 'Etc/UTC'; // UTC
 
 // Root path for file manager
 // use absolute path of directory i.e: '/var/www/folder' or $_SERVER['DOCUMENT_ROOT'].'/folder'
-$root_path =  /home/rsupsoer/';
+$root_path =  '/home/rsupsoer/';
 
 // Root url for links in file manager.Relative to $http_host. Variants: '', 'path/to/subfolder'
 // Will not working if $root_path will be outside of server document root
