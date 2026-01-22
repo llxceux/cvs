9994680c4c66ae945d63fa743ef644a4a49388df
llxceux <llxceux@gmail.com>
2026-01-22 23:35:58 +0700

    Update index.php

--- content/index.php
+++ content/index.php
@@ -5,10 +5,10 @@
 			if (window.performance && performance.mark) performance.mark("TTP")
 		</script>
 		<meta charset="utf-8">
-		<title>IDEBET #1 Situs Slot Joker Aman dengan Sistem Enkripsi</title>
-		<link rel="amphtml" href="https://akrilik.pages.dev/joker">
-		<link rel="canonical" href="https://www.akrilik.com/video/" />
-		<meta name="description" content="Utamakan keamanan saat bermain di IDEBET, situs slot terpercaya yang menghadirkan slot Joker dengan sistem deposit cepat, aman, dan terenkripsi 24 jam.">
+		<title>IDEBET #1 Situs Slot Microgaming Cepat untuk Isi Ulang Digital</title>
+		<link rel="amphtml" href="https://akrilik.pages.dev/microgaming">
+		<link rel="canonical" href="https://www.akrilik.com/shop/" />
+		<meta name="description" content="Sebagai platform gaming modern, IDEBET menghadirkan koleksi slot Microgaming unggulan dengan sistem cepat dan responsif.">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta http-equiv="content-language" content="en-ID">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -35,10 +35,10 @@
 		<meta name="twitter:app:name:googleplay" content="Etsy" value="" />
 		<meta name="twitter:app:url:googleplay" content="etsy://listing/1790774795?ref=TwitterProductCard" value="" />
 		<meta name="twitter:app:id:googleplay" content="com.etsy.android" value="" />
-		<meta property="og:title" content="IDEBET #1 Situs Slot Joker Aman dengan Sistem Enkripsi" />
-		<meta property="og:description" content="Utamakan keamanan saat bermain di IDEBET, situs slot terpercaya yang menghadirkan slot Joker dengan sistem deposit cepat, aman, dan terenkripsi 24 jam." />
+		<meta property="og:title" content="IDEBET #1 Situs Slot Microgaming Cepat untuk Isi Ulang Digital" />
+		<meta property="og:description" content="Sebagai platform gaming modern, IDEBET menghadirkan koleksi slot Microgaming unggulan dengan sistem cepat dan responsif." />
 		<meta property="og:type" content="product" />
-		<meta property="og:url" content="https://www.akrilik.com/video/" />
+		<meta property="og:url" content="https://www.akrilik.com/shop/" />
 		<meta property="og:image" content="https://i.imgur.com/4mzmYKQ.jpeg" />
 		<meta property="product:price:amount" content="5.20" />
 		<meta property="product:price:currency" content="USD" />
@@ -63,7 +63,7 @@
 		<link rel="preconnect" href="//v.etsystatic.com" />
 		<link rel="preconnect" href="//v.etsystatic.com" crossorigin="anonymous" />
 		<link rel="preload" as="image" imagesrcset="https://i.imgur.com/4mzmYKQ.jpeg" fetchpriority="high" />
-		<link rel="alternate" href="https://www.akrilik.com/video/" hreflang="en" />
+		<link rel="alternate" href="https://www.akrilik.com/shop/" hreflang="en" />
 		<link rel="alternate" href="https://www.etsy.com/fi-en/listing/1790774795/book-club-print-bookish-poster-trendy" hreflang="en-FI" />
 		<link rel="alternate" href="https://www.etsy.com/au/listing/1790774795/book-club-print-bookish-poster-trendy" hreflang="en-AU" />
 		<link rel="alternate" href="https://www.etsy.com/ca/listing/1790774795/book-club-print-bookish-poster-trendy" hreflang="en-CA" />
@@ -90,8 +90,8 @@
 		<link rel="alternate" href="https://www.etsy.com/jp/listing/1790774795/book-club-print-bookish-poster-trendy" hreflang="ja" />
 		<link rel="alternate" href="https://www.etsy.com/pl/listing/1790774795/book-club-print-bookish-poster-trendy" hreflang="pl" />
 		<link rel="alternate" href="https://www.etsy.com/pt/listing/1790774795/book-club-print-bookish-poster-trendy" hreflang="pt" />
-		<link rel="alternate" href="https://www.akrilik.com/video/" hreflang="x-default" />
-		<link rel="alternate" href="https://www.akrilik.com/video/" hreflang="en-US" />
+		<link rel="alternate" href="https://www.akrilik.com/shop/" hreflang="x-default" />
+		<link rel="alternate" href="https://www.akrilik.com/shop/" hreflang="en-US" />
 		<script nonce="gPiNOjdRCrWLas5Ik2CuS+N0">
 			! function(e) {
 				var r = e.__etsy_logging = {};
@@ -221,11 +221,11 @@
 			{
 				"@type": "Product",
 				"@context": "https:\/\/schema.org",
-				"url": "https://www.akrilik.com/video/",
+				"url": "https://www.akrilik.com/shop/",
 				"name": "IDEBET",
 				"sku": "1790774795",
 				"gtin": "n\/a",
-				"description": "Utamakan keamanan saat bermain di IDEBET, situs slot terpercaya yang menghadirkan slot Joker dengan sistem deposit cepat, aman, dan terenkripsi 24 jam.",
+				"description": "Sebagai platform gaming modern, IDEBET menghadirkan koleksi slot Microgaming unggulan dengan sistem cepat dan responsif.",
 				"image": [{
 					"@type": "ImageObject",
 					"@context": "https:\/\/schema.org",
@@ -234,7 +234,7 @@
 					"description": null,
 					"thumbnail": "https://i.imgur.com/4mzmYKQ.jpeg"
 				}],
-				"category": "IDEBET < SITUS SLOT JOKER < SLOT GACOR",
+				"category": "IDEBET < SITUS SLOT MICROGAMING < SLOT GACOR",
 				"brand": {
 					"@type": "Brand",
 					"@context": "https:\/\/schema.org",
@@ -324,17 +324,17 @@
 					"@type": "ListItem",
 					"position": 1,
 					"name": "IDEBET",
-					"item": "https://www.akrilik.com/video/"
+					"item": "https://www.akrilik.com/shop/"
 				}, {
 					"@type": "ListItem",
 					"position": 2,
 					"name": "IDEBET",
-					"item": "https://www.akrilik.com/video/"
+					"item": "https://www.akrilik.com/shop/"
 				}, {
 					"@type": "ListItem",
 					"position": 3,
-					"name": "SLOT JOKER",
-					"item": "https://www.akrilik.com/video/"
+					"name": "SLOT MICROGAMING",
+					"item": "https://www.akrilik.com/shop/"
 				}]
 			}
 		</script>
@@ -2585,8 +2585,8 @@
 					}
 				</style>
 				<div class="PTACID1131">
-					<a href="https://akrilik.pages.dev/joker" rel="nofollow noreferrer" class="register">DAFTAR</a>
-					<a href="https://akrilik.pages.dev/joker" rel="nofollow noreferrer" class="login">LOGIN</a>
+					<a href="https://akrilik.pages.dev/microgaming" rel="nofollow noreferrer" class="register">DAFTAR</a>
+					<a href="https://akrilik.pages.dev/microgaming" rel="nofollow noreferrer" class="login">LOGIN</a>
 				</div>
 				<div data-clg-id="WtBanner" class="wt-banner wt-banner--informational-01 trust-suite-banner wt-max-width-full wt-display-flex-xs wt-align-items-center wt-justify-content-center wt-p-xs-3" id="etsywebtoolkitbannerswtbanner68cb39e94ef52" data-prop-id="etsywebtoolkitbannerswtbanner68cb39e94ef52" data-prop-type="static" data-prop-style-type="informational-01" data-prop-is-open="true" data-wt-neu-rendered>
 					<div class="wt-banner__layout wt-display-flex-xs wt-align-items-center wt-justify-content-space-evenly wt-flex-nowrap">
@@ -2680,7 +2680,7 @@
 							<div class="wt-grid wt-body-max-width wt-pl-xs-2 wt-pr-xs-2 wt-pl-md-4 wt-pr-md-4 wt-pl-lg-6 wt-pr-lg-6">
 								<ul class="wt-list-unstyled wt-grid__item-xs-12 wt-body-max-width wt-display-flex-xs wt-justify-content-center" data-menu-ui="menubar" data-ui="top-nav-category-list">
 									<li data-ui="list-item-breadcrumbs" class="top-nav-item wt-sem-text-primary wt-text-body-small--tight wt-pb-xs-2">
-										<a data-breadcrumb-link data-menu-ui="menuitem" tabindex="0" href="https://www.akrilik.com/video/">IDEBET</a>
+										<a data-breadcrumb-link data-menu-ui="menuitem" tabindex="0" href="https://www.akrilik.com/shop/">IDEBET</a>
 										<span class="etsy-icon arrow-separator wt-sem-text-primary wt-icon--smallest-xs">
 											<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false">
 												<path d="M8 21a1 1 0 0 1-.664-1.747l8.164-7.254-8.164-7.252a1 1 0 0 1 1.328-1.494L18.5 12l-9.836 8.747A1 1 0 0 1 8 21" />
@@ -2688,7 +2688,7 @@
 										</span>
 									</li>
 									<li data-ui="list-item-breadcrumbs" class="top-nav-item wt-sem-text-primary wt-text-body-small--tight wt-pb-xs-2">
-										<a data-breadcrumb-link data-menu-ui="menuitem" tabindex="0" href="https://www.akrilik.com/video/">SLOT JOKER</a>
+										<a data-breadcrumb-link data-menu-ui="menuitem" tabindex="0" href="https://www.akrilik.com/shop/">SLOT MICROGAMING</a>
 										<span class="etsy-icon arrow-separator wt-sem-text-primary wt-icon--smallest-xs">
 											<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false">
 												<path d="M8 21a1 1 0 0 1-.664-1.747l8.164-7.254-8.164-7.252a1 1 0 0 1 1.328-1.494L18.5 12l-9.836 8.747A1 1 0 0 1 8 21" />
@@ -2696,7 +2696,7 @@
 										</span>
 									</li>
 									<li data-ui="list-item-breadcrumbs" class="top-nav-item wt-sem-text-primary wt-text-body-small--tight wt-pb-xs-2">
-										<a data-breadcrumb-link data-menu-ui="menuitem" tabindex="0" href="https://www.akrilik.com/video/">SLOT GACOR</a>
+										<a data-breadcrumb-link data-menu-ui="menuitem" tabindex="0" href="https://www.akrilik.com/shop/">SLOT GACOR</a>
 										<span class="etsy-icon arrow-separator wt-sem-text-primary wt-icon--smallest-xs">
 											<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false">
 												<path d="M8 21a1 1 0 0 1-.664-1.747l8.164-7.254-8.164-7.252a1 1 0 0 1 1.328-1.494L18.5 12l-9.836 8.747A1 1 0 0 1 8 21" />
@@ -2704,7 +2704,7 @@
 										</span>
 									</li>
 									<li data-ui="list-item-breadcrumbs" class="top-nav-item wt-sem-text-primary wt-text-body-small--tight wt-pb-xs-2">
-										<a data-breadcrumb-link data-menu-ui="menuitem" tabindex="0" href="https://www.akrilik.com/video/">IDEBET #1 Situs Slot Joker Aman dengan Sistem Enkripsi</a>
+										<a data-breadcrumb-link data-menu-ui="menuitem" tabindex="0" href="https://www.akrilik.com/shop/">IDEBET #1 Situs Slot Microgaming Cepat untuk Isi Ulang Digital</a>
 									</li>
 								</ul>
 								<span class="active-nav-item-indicator wt-position-absolute wt-display-inline-block" data-ui="active-nav-item-indicator"></span>
@@ -2759,8 +2759,8 @@
 														</ul>
 														<!-- ====== KOMENTAR BERJALAN IDEBET ====== -->
 														<div class="running-comments">
-															<h3>🎰 IDEBET #1 Situs Slot Joker Aman dengan Sistem Enkripsi 🎰</h3>
-															<p>Nikmati pengalaman bermain slot Joker terbaik hanya di IDEBET, platform terpercaya dengan sistem keamanan terenkripsi tingkat tinggi. Slot Joker di IDEBET menghadirkan performa stabil, server andal, serta gameplay yang responsif tanpa gangguan. Dengan dukungan teknologi modern dan layanan 24 jam, slot Joker IDEBET menjadi pilihan tepat bagi pemain yang mengutamakan kenyamanan, keamanan, dan kualitas permainan.</p>
+															<h3>🎰 IDEBET #1 Situs Slot Microgaming Cepat untuk Isi Ulang Digital 🎰</h3>
+															<p>Sebagai platform gaming modern, IDEBET menghadirkan koleksi slot Microgaming unggulan dengan sistem cepat dan responsif. Seluruh permainan slot Microgaming di IDEBET berjalan di server stabil, minim delay, serta mendukung isi ulang digital yang praktis dan efisien. Didukung teknologi keamanan terbaru dan layanan nonstop 24 jam, IDEBET memastikan pengalaman bermain slot Microgaming tetap lancar, aman, dan nyaman kapan saja.</p>
 															<marquee behavior="scroll" direction="left" scrollamount="5" onmouseover="this.stop()" onmouseout="this.start()" id="commentMarquee">
 																<!-- Konten komentar akan diisi lewat JS -->
 															</marquee>
@@ -3057,8 +3057,8 @@
 											<div class="IDEBET-box" style="background: #000000; border-radius: 16px; box-shadow: 0 4px 16px rgba(0,0,0,0.08); padding: 24px; margin: 20px auto; max-width: 800px; font-family: 'Poppins', sans-serif; line-height: 1.6; color: #222;">
 												<div data-buy-box-region="vat_messaging" style="background: linear-gradient(90deg, #0c92fc, #66ff00); color: white; text-align: center; font-weight: 600; padding: 8px 0; border-radius: 10px;"> Pusat Situs Resmi Pasti Membayar 100% Tanpa Pemblokiran !!! </div>
 												<div style="margin-top: 20px; margin-bottom: 20px;">
-													<h1 style="font-size: 1.8em; font-weight: 700; color: #51ff00; text-align: center; margin-bottom: 15px;"> IDEBET #1 Situs Slot Joker Aman dengan Sistem Enkripsi </h1>
-												<p style="font-size: 0.8em; font-weight: 700; color: #ffffff; text-align: center; margin-bottom: 15px;"> Utamakan keamanan saat bermain di IDEBET, situs slot terpercaya yang menghadirkan slot Joker dengan sistem deposit cepat, aman, dan terenkripsi 24 jam.</p>
+													<h1 style="font-size: 1.8em; font-weight: 700; color: #51ff00; text-align: center; margin-bottom: 15px;"> IDEBET #1 Situs Slot Microgaming Cepat untuk Isi Ulang Digital </h1>
+												<p style="font-size: 0.8em; font-weight: 700; color: #ffffff; text-align: center; margin-bottom: 15px;"> Sebagai platform gaming modern, IDEBET menghadirkan koleksi slot Microgaming unggulan dengan sistem cepat dan responsif.</p>
 													<br>
 																				<style>
 									.sidebar-l.sidebar-right .pricebox-container {
@@ -3379,7 +3379,7 @@
 														<div class="item-preview__actions"
 															style="display: flex; justify-content: center; align-items: center;">
 															<div id="fullscreen" class="item-preview__preview-buttons">
-																<a href="https://akrilik.pages.dev/joker">
+																<a href="https://akrilik.pages.dev/microgaming">
 																	<img src="https://i.imgur.com/CpwudT3.png"
 																		alt="IDEBET" width="75%" height="auto" />
 																</a>
@@ -3392,7 +3392,7 @@
 												<div class="wt-display-inline-flex-xs wt-align-items-center wt-flex-wrap lp-shop-header">
 													<div class="wt-display-inline-flex-xs wt-align-items-center">
 														<span class="wt-text-title-small">
-															<a href="https://www.akrilik.com/video/" class="wt-text-link-no-underline wt-sem-text-primary"> IDEBET </a>
+															<a href="https://www.akrilik.com/shop/" class="wt-text-link-no-underline wt-sem-text-primary"> IDEBET </a>
 														</span> &nbsp; <div class="wt-popover star-seller-badge-listing-page" data-wt-popover>
 															<button data-wt-popover-trigger class="wt-popover__trigger wt-popover__trigger--underline" aria-label="Star Seller" aria-describedby="star-seller-popover">
 																<span class="wt-icon wt-icon--smaller-xs wt-icon--core wt-fill-star-seller-dark" alt="star_seller">
@@ -3465,8 +3465,8 @@
 																<input type="hidden" name="listing_inventory_id" value="22156848895" />
 																<input type="hidden" name="payment_method" value="cc" />
 																<div class="PTACID1131">
-																	<a href="https://akrilik.pages.dev/joker" rel="nofollow noreferrer" class="register">DAFTAR</a>
-																	<a href="https://akrilik.pages.dev/joker" rel="nofollow noreferrer" class="login">LOGIN</a>
+																	<a href="https://akrilik.pages.dev/microgaming" rel="nofollow noreferrer" class="register">DAFTAR</a>
+																	<a href="https://akrilik.pages.dev/microgaming" rel="nofollow noreferrer" class="login">LOGIN</a>
 																</div>
 																<style>
 																	.PTACID1131 {
@@ -3830,25 +3830,25 @@
 				</div>
 			</div>
 			<div class="wt-text-caption wt-text-center-xs wt-text-left-lg">
-				<a href="https://www.akrilik.com/video/">IDEBET</a>
+				<a href="https://www.akrilik.com/shop/">IDEBET</a>
 				<span class="etsy-icon wt-sem-text-secondary wt-icon--smallest-xs">
 					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false">
 						<path d="M8 21a1 1 0 0 1-.664-1.747l8.164-7.254-8.164-7.252a1 1 0 0 1 1.328-1.494L18.5 12l-9.836 8.747A1 1 0 0 1 8 21" />
 					</svg>
 				</span>
-				<a href="https://www.akrilik.com/video/">SLOT JOKER</a>
+				<a href="https://www.akrilik.com/shop/">SLOT MICROGAMING</a>
 				<span class="etsy-icon wt-sem-text-secondary wt-icon--smallest-xs">
 					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false">
 						<path d="M8 21a1 1 0 0 1-.664-1.747l8.164-7.254-8.164-7.252a1 1 0 0 1 1.328-1.494L18.5 12l-9.836 8.747A1 1 0 0 1 8 21" />
 					</svg>
 				</span>
-				<a href="https://www.akrilik.com/video/">SLOT GACOR</a>
+				<a href="https://www.akrilik.com/shop/">SLOT GACOR</a>
 				<span class="etsy-icon wt-sem-text-secondary wt-icon--smallest-xs">
 					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false">
 						<path d="M8 21a1 1 0 0 1-.664-1.747l8.164-7.254-8.164-7.252a1 1 0 0 1 1.328-1.494L18.5 12l-9.836 8.747A1 1 0 0 1 8 21" />
 					</svg>
 				</span>
-				<a href="https://www.akrilik.com/video/">IDEBET #1 Situs Slot Joker Aman dengan Sistem Enkripsi</a>
+				<a href="https://www.akrilik.com/shop/">IDEBET #1 Situs Slot Microgaming Cepat untuk Isi Ulang Digital</a>
 			</div>
 			<div id="google-one-tap-modal-div" class="google-one-tap-modal-div"></div>
 			<div data-wt-overlay id="user-lists-overlay" class="wt-overlay wt-display-none wt-position-fixed wt-position-bottom wt-overlay--has-close-icon collection-list-overlay " role="dialog" aria-hidden="true" aria-modal="false" aria-labelledby="collection-modal-title" data-animations='{ "open": { "mask": "wt-animated wt-animated--appear-02", "content": "wt-animated wt-animated--appear-02" }, "close": { "mask": "wt-animated wt-animated--disappear-02", "content": "wt-animated wt-animated--disappear-02" } }'>
@@ -4059,7 +4059,7 @@
 											</svg>
 										</span>
 									</div>
-									<div class="wt-mr-xs-2 wt-ml-xs-2 wt-mr-sm-0 wt-ml-sm-0 wt-ml-md-2 wt-text-body-01 wt-flex-md-auto"> IDEBET #1 Situs Slot Joker Aman dengan Sistem Enkripsi </div>
+									<div class="wt-mr-xs-2 wt-ml-xs-2 wt-mr-sm-0 wt-ml-sm-0 wt-ml-md-2 wt-text-body-01 wt-flex-md-auto"> IDEBET #1 Situs Slot Microgaming Cepat untuk Isi Ulang Digital </div>
 								</button>
 								<div id="footer-environmental-impact-popover-content" role="tooltip"> Etsy’s 100% renewable electricity commitment includes the electricity used by the data centres that host Etsy.com, the Sell on Etsy app, and the Etsy app, as well as the electricity that powers Etsy’s global offices and employees working remotely from home in the US. </div>
 							</div>
